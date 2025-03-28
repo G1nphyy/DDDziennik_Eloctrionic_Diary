a0f4f4561b7837952ff42b1a051be0ab7a5717e0
G1nphy <161763449+G1nphyy@users.noreply.github.com>
2025-03-28 21:59:25 +0100

    Updated

---
+++ app/Models/Students_tests_homeworks.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Students_tests_homeworks extends Model
+{
+    /** @use HasFactory<\Database\Factories\StudentsTestsHomeworksFactory> */
+    use HasFactory;
+}
---
+++ database/factories/TestsHomeworksFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tests_homeworks>
+ */
+class TestsHomeworksFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
